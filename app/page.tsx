7bdd41ac1933ebee577923df610d27bd36f728b5
Jorge Schneider <jorgeschneidr@gmail.com>
2026-05-07 18:07:14 -0300

    Initial landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,56 @@
 import Image from "next/image";
+import {
+  Dot,
+  ArrowRight
+} from 'lucide-react'
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <main
+      className="relative min-h-screen bg-cover bg-center flex items-center justify-center overflow-hidden"
+      style={{
+        backgroundImage: "url('/hero-bg.png')",
+      }}
+    >
+      <div className="absolute inset-0 bg-black/20 backdrop-blur-[2px]" />
+
+      <div className="absolute top-0 left-0 w-full z-20 flex justify-center pt-8">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          src="/logo.svg"
+          alt="Logo"
+          width={180}
+          height={60}
+          className="drop-shadow-[0_0_25px_rgba(139,92,246,0.35)]"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+      </div>
+
+      <div className="relative z-10 flex flex-col items-center justify-center text-center px-6 max-w-5xl">
+
+        <h1 className="text-white lg:text-6xl md:text-6xl text-3xl font-bold tracking-tight leading-[0.95] mt-4">
+          Outbound is broken. We fix it.
+        </h1>
+        <h1 className="text-violet-700 text-2xl lg:text-6xl md:text-5xl font-bold tracking-tight leading-[0.95] mt-4">
+          Lead Generation, Reimagined.
+        </h1>
+
+        <p className="text-gray-200 text-sm lg:text-1xl md:text-xl mt-12 leading-relaxed">
+          Cold calls and emails are dead. Just like your pipeline.
+        </p>
+        <p className="text-gray-200 text-sm lg:text-1xl md:text-xl leading-relaxed">
+          AI + social + creative to drive fresh, qualified leads.
+        </p>
+
+        <button className="mt-14 px-8 py-4 flex items-center justify-center gap-2 rounded-lg bg-violet-600 hover:bg-violet-800 w-sm transition-all duration-300 text-white font-bold shadow-lg shadow-violet-600/30 cursor-pointer">
+          Whislist
+          <ArrowRight className="ml-2" />
+        </button>
+        <div className="mt-4 flex items-center gap-2 text-gray-500 font-medium">
+          <p>Setup in minutes</p>
+          <Dot size={30} />
+          <p>Cancel anytime</p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+
+      </div>
+    </main>
   );
 }
