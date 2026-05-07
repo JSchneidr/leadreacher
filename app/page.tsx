90068ad43f295b1706d99f18cbcff012652ebfb2
Jorge Schneider <jorgeschneidr@gmail.com>
2026-05-07 20:04:33 -0300

    font update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import {
-  Dot,
   ArrowRight
 } from 'lucide-react'
 
@@ -12,7 +11,7 @@ export default function Home() {
         backgroundImage: "url('/hero-bg.png')",
       }}
     >
-      <div className="absolute inset-0 bg-black/20 backdrop-blur-[2px]" />
+      <div className="absolute inset-0 bg-black/15 backdrop-blur-[2px]" />
 
       <div className="absolute top-0 left-0 w-full z-20 flex justify-center pt-8">
         <Image
@@ -26,30 +25,22 @@ export default function Home() {
 
       <div className="relative z-10 flex flex-col items-center justify-center text-center px-6 max-w-5xl">
 
-        <h1 className="text-white lg:text-6xl md:text-6xl text-3xl font-bold tracking-tight leading-[0.95] mt-4">
+        <h1 className="text-gray-900 lg:text-6xl md:text-6xl text-3xl font-bold tracking-tight leading-[0.95] mt-4">
           Outbound is broken. We fix it.
         </h1>
-        <h1 className="text-violet-700 text-2xl lg:text-6xl md:text-5xl font-bold tracking-tight leading-[0.95] mt-4">
+        <h1 className="text-violet-800 text-2xl lg:text-5xl md:text-5xl font-bold tracking-tight leading-[0.95] mt-4">
           Lead Generation, Reimagined.
         </h1>
 
-        <p className="text-gray-200 text-sm lg:text-1xl md:text-xl mt-12 leading-relaxed">
-          Cold calls and emails are dead. Just like your pipeline.
-        </p>
-        <p className="text-gray-200 text-sm lg:text-1xl md:text-xl leading-relaxed">
-          AI + social + creative to drive fresh, qualified leads.
-        </p>
-
-        <button className="mt-14 px-8 py-4 flex items-center justify-center gap-2 rounded-lg bg-violet-600 hover:bg-violet-800 w-sm transition-all duration-300 text-white font-bold shadow-lg shadow-violet-600/30 cursor-pointer">
+        <div className="text-gray-500 text-xs lg:text-xl md:text-lg mt-12 leading-relaxed">
+          <p>Cold calls and emails are dead. Just like your pipeline.</p>
+          <p>AI + social + creative to drive fresh, qualified leads.</p>
+        </div>
+        
+        <button className="mt-14 px-20 py-5 flex items-center justify-center gap-2 rounded-lg bg-violet-800 hover:bg-violet-900 w-sm transition-all duration-300 text-gray-50 font-bold shadow-lg shadow-violet-600/30 cursor-pointer">
           Whislist
           <ArrowRight className="ml-2" />
         </button>
-        <div className="mt-4 flex items-center gap-2 text-gray-500 font-medium">
-          <p>Setup in minutes</p>
-          <Dot size={30} />
-          <p>Cancel anytime</p>
-        </div>
-
       </div>
     </main>
   );
